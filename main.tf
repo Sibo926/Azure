2831fde0d3512325d2cff09ff5ba3ec8921af4cd
Sibo926 <78701183+Sibo926@users.noreply.github.com>
2021-12-03 11:25:54 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.86.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "ResourceGroup1"
+  location = "West Europe"
+}
